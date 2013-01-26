c56aba77a60f485e82b345b30ef5cfe84ef27cdd
tigawa <ihciiat@gmail.com>
2013-01-26 17:23:54 +0900

    first commted

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Merman::Application.config.secret_token = 'a5856ebd853f8b2ba825e61afa872ad8148d41e2706afa92d52192a209e1d63947a3e15fbb882af582a0618dcee7235ce807d8cce7fb5702f440e7ea74c39a23'
